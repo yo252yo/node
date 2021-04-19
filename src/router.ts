47ba5f1397e122ed62ca12cda6e459f909efedde
yoann <yo252yo@gmail.com>
2021-04-19 13:59:35 +0200

    adds example serving file directly

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,10 @@
 import express  from "express";
 export const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/template", (req, res) => {
   res.render("template", {});
 } );
+
+router.get('/html', function(req, res, next){
+  res.sendfile('./src/static/html.html');
+});
